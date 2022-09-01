251d4a62710f891a53ef278bd3ba6968cfc9eeb7
Robson Braga de Queiroz <rbq.dev@gmail.com>
2022-08-31 23:52:37 -0300

    feat: update type and modal events

--- src/components/ModalEvent/ModalEvent.tsx
+++ src/components/ModalEvent/ModalEvent.tsx
@@ -11,17 +11,74 @@ import {
   Input,
   ModalFooter,
   Flex,
+  useToast,
 } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { format } from "date-fns";
+import type { Event } from "src/types";
 
 type ModalEventProps = {
   isOpen: boolean;
   onClose: () => void;
+  onSaveEvent: (event: Event) => void;
 };
 
-export const ModalEvent = ({ isOpen, onClose }: ModalEventProps) => {
+export enum InputEvent {
+  NAME = "name",
+  DATE = "date",
+  START_TIME = "startTime",
+  END_TIME = "endTime",
+}
+
+const DEFAULT_VALUES: Record<InputEvent, any> = {
+  [InputEvent.NAME]: "",
+  [InputEvent.DATE]: format(new Date(), "yyyy-MM-dd"),
+  [InputEvent.START_TIME]: "",
+  [InputEvent.END_TIME]: "",
+};
+
+export const ModalEvent = ({
+  isOpen,
+  onClose,
+  onSaveEvent,
+}: ModalEventProps) => {
   const initialRef = useRef(null);
+  const [values, setValues] = useState(DEFAULT_VALUES);
+  const toast = useToast();
+
+  const formIsInvalid = () => {
+    return Object.keys(values).some((key) => !values[key as InputEvent]);
+  };
+
+  const verifyTime = () => {
+    if (values[InputEvent.START_TIME] > values[InputEvent.END_TIME]) {
+      toast({
+        title: `The start time can't be bigger than end time`,
+        status: "error",
+        isClosable: true,
+      });
+
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    if (!verifyTime()) {
+      return;
+    }
+
+    onSaveEvent(values);
+    setValues({ ...DEFAULT_VALUES });
+    onClose();
+  };
 
   return (
     <>
@@ -38,7 +95,14 @@ export const ModalEvent = ({ isOpen, onClose }: ModalEventProps) => {
           <ModalBody>
             <FormControl>
               <FormLabel>Event name</FormLabel>
-              <Input ref={initialRef} placeholder="Event name" maxLength={30} />
+              <Input
+                ref={initialRef}
+                placeholder="Event name"
+                maxLength={30}
+                name={InputEvent.NAME}
+                value={values[InputEvent.NAME]}
+                onChange={handleChange}
+              />
             </FormControl>
 
             <FormControl mt={4}>
@@ -47,25 +111,47 @@ export const ModalEvent = ({ isOpen, onClose }: ModalEventProps) => {
                 placeholder="Select Date and Time"
                 size="md"
                 type="date"
-                defaultValue={format(new Date(), "yyyy-MM-dd")}
+                name={InputEvent.DATE}
+                defaultValue={values[InputEvent.DATE]}
+                onChange={handleChange}
               />
             </FormControl>
 
             <Flex gap={4}>
               <FormControl mt={4}>
                 <FormLabel>Start Time</FormLabel>
-                <Input placeholder="Start Time" size="md" type="time" />
+                <Input
+                  placeholder="Start Time"
+                  size="md"
+                  type="time"
+                  name={InputEvent.START_TIME}
+                  value={values[InputEvent.START_TIME]}
+                  onChange={handleChange}
+                />
               </FormControl>
 
               <FormControl mt={4}>
                 <FormLabel>End Time</FormLabel>
-                <Input placeholder="End Time" size="md" type="time" />
+                <Input
+                  placeholder="End Time"
+                  size="md"
+                  type="time"
+                  name={InputEvent.END_TIME}
+                  value={values[InputEvent.END_TIME]}
+                  onChange={handleChange}
+                />
               </FormControl>
             </Flex>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              type="button"
+              onClick={handleSubmit}
+              disabled={formIsInvalid()}
+            >
               Save
             </Button>
             <Button onClick={onClose}>Cancel</Button>
---
+++ src/types/events.d.ts
@@ -0,0 +1,6 @@
+export type Event = {
+  name: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+};
